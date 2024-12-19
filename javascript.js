b570c6b1a5b460471ab43be6f91128b6b151957c
Sean Alkaline <144847524+Sean-Whizzy@users.noreply.github.com>
2024-12-19 11:41:09 -0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,10 @@
+/* JavaScript */
+/* Toggle side menu visibility */
+function toggleMenu() {
+    const sideMenu = document.getElementById("sideMenu");
+    if (sideMenu.style.display === "block") {
+        sideMenu.style.display = "none";
+    } else {
+        sideMenu.style.display = "block";
+    }
+}
